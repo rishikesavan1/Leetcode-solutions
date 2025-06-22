70cda70d5c523d1fc82aada5dff83e49ec180c4e
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-06-22 23:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (34.15%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.cpp
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     bool isBalanced(string num) {
         int evensum = 0 , oddsum = 0;
-        for(int i = 0 ; i < num.size() ; i++){
-            if(i%2)
+        for(int i = 0 ; i < num.size() ; i++)
+            if(i%2) 
                 evensum += '0' - num[i];
             else
-                oddsum += '0' - num[i];
-        }
+                oddsum += '0' - num[i];     
         return oddsum == evensum;
     }
 };
