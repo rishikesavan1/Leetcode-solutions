cd4988f16aab6c7f5e3e7f79383431a5315cc263
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-06-24 11:41:31 +0530

    Time: 862 ms (90.81%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,16 +1,12 @@
-# Write your MySQL query statement below
-SELECT
-    d.name AS Department,
-    e.name AS Employee,
-    e.salary AS Salary
-FROM
-    Employee e
-    JOIN Department d ON e.departmentId = d.id
-WHERE
-    (
-        SELECT COUNT(DISTINCT salary)
-        FROM Employee e2
-        WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
-    ) <= 3
-ORDER BY
-    Department, Salary DESC;
+with cte as(
+select
+id,
+name,
+salary,
+departmentId,
+dense_rank() over(partition by departmentId order by salary desc) as top_salaries
+from Employee
+)
+
+select d.name as Department, c.name as Employee, c.salary from Department d join cte c on 
+d.id = c.departmentId where top_salaries <=3
