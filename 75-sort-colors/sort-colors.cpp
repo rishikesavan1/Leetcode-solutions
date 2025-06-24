980c8086b0a30a660b24e975f66930081503d6f3
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-06-24 18:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (78.86%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int low = 0 , mid = 0 , high = nums.size()-1;
-        while(mid <= high){
-            if(nums[mid] == 0){
+        while(mid <= high)
+            if(nums[mid] == 0)
                 swap(nums[mid++],nums[low++]);
-            }
-            else if(nums[mid] == 2){
+            else if(nums[mid] == 2)
                 swap(nums[mid],nums[high--]);
-            }
             else mid++;
-        }
     }
 };
