349c404716e7a992e073ed7f5f7e7449dd1b8703
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-06-25 18:22:09 +0530

    Time: 676 ms (10.27%) | Memory: 98 MB (90.33%) - LeetSync

--- 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,55 +1,37 @@
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-    struct ___ {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
-#endif
-
 class Solution {
 public:
-    using ll=long long;
-
-    ll kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, ll k) {
-        const int &n1=nums1.size(), &n2=nums2.size();
-        ll l=-1e10, r=1e10, mid;
-
-        auto kthNotGreaterValue=[&](const ll &x)-> bool{
-            ll cnt=0;
-            for(int i=0;i<n1;++i){
-                if(nums1[i]<0){
-                    auto it=lower_bound(nums2.begin(), nums2.end(), (ll)ceil(1.0*x/nums1[i]));
-                    cnt+=distance(it, nums2.end());
-                }
-                else if(nums1[i]>0){
-                    auto it=upper_bound(nums2.begin(), nums2.end(), (ll)floor(1.0*x/nums1[i]));
-                    cnt+=distance(nums2.begin(), it);
-                }
-                else if(x>=0)
-                    cnt+=n2;
-                
-                if(cnt>=k)
-                    return 1;
-            }
-
-            return cnt>=k;
-        };
-
-        ll ans=0LL;
-        while(l<=r){
-            mid=l+(r-l)/2;
-            
-            if(kthNotGreaterValue(mid)){
-                ans=mid;
-                r=mid-1;
-            }
-            else
-                l=mid+1;
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        long long left = -1e10, right = 1e10;
+        while (left < right) {
+            long long mid = left + (right - left) / 2;
+            if (countProducts(nums1, nums2, mid) < k) left = mid + 1;
+            else right = mid;
         }
-        
-        return ans;
+        return left;
+    }
+
+    long long countProducts(vector<int>& nums1, vector<int>& nums2, long long target) {
+        long long count = 0;
+        for (int num1 : nums1) {
+            if (num1 == 0) {
+                if (target >= 0) count += nums2.size();
+                continue;
+            }
+
+            int low = 0, high = nums2.size();
+            while (low < high) {
+                int mid = (low + high) / 2;
+                long long prod = 1LL * num1 * nums2[mid];
+                if (prod <= target) {
+                    if (num1 > 0) low = mid + 1;
+                    else high = mid;
+                } else {
+                    if (num1 > 0) high = mid;
+                    else low = mid + 1;
+                }
+            }
+            count += (num1 > 0) ? low : (nums2.size() - low);
+        }
+        return count;
     }
 };
