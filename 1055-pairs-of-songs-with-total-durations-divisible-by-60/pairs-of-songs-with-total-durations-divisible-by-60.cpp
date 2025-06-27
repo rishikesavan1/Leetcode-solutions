bfebf33c2db20e085a6af9a37d430eca6090cb42
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-06-27 12:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (70.39%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -5,7 +5,8 @@ public:
         vector<int> tyme(61,0);
         for(auto i : time){
             int temp = i%60;
-            if(!temp)   count += tyme[0];
+            if(temp == 0)   
+                count += tyme[0];
             else    count += tyme[60-temp];
             tyme[temp]++;
         }
