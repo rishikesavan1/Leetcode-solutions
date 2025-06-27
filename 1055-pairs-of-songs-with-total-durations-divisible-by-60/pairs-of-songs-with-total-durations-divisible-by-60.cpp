ed3d34bad39dd99881d86a9e7aa7fd65e608d220
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-06-27 12:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (88.47%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
         int count = 0;
-        vector<int> tyme(61,0);
+        vector<int> remtime(61,0);
         for(auto i : time){
             int temp = i%60;
             if(temp == 0)   
-                count += tyme[0];
-            else    count += tyme[60-temp];
-            tyme[temp]++;
+                count += remtime[0];
+            else    
+                count += remtime[60-temp];
+            remtime[temp]++;
         }
         return count;
     }
