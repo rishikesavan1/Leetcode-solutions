aad483c560d0f482a77806315ba5f7f7a3483151
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-06-30 16:24:45 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (60.26%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -3,8 +3,12 @@ public:
     int maxProfit(vector<int>& prices) {
         int minprice = INT_MAX , maxprofit = 0 ;
         for(auto i : prices){
-            minprice = min(minprice , i);
-            maxprofit = max( i - minprice ,maxprofit);
+            if(minprice > i){
+                minprice = i;
+            }
+            else if(maxprofit < i - minprice){
+                maxprofit = i - minprice;
+            }
         }
         return maxprofit;
     }
