11c8575e26795cbcb398058a9ff62d0318914545
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-07-01 18:52:51 +0530

    Time: 2 ms (48.46%) | Memory: 8.9 MB (29.69%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int possibleStringCount(string word) {
         int count = 1;
-        for(char i = 0 ; i < word.size()-1;i++)
+        for(char i = 0 ; i < word.size()-1;i++) // char works in terms of ASCII value starts from 0 to 127 it optimize the space and time
             if(word[i] == word[i+1])
                 count++;
         return count;
