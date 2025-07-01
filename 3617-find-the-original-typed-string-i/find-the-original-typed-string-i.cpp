50917698adf252ae0362f775121fe244607bb970
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-07-01 18:47:15 +0530

    Time: 4 ms (27.73%) | Memory: 8.9 MB (60.22%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count = 1;
+        vector<int> arr(26,0);
+        for(int i = 0 ; i < word.size()-1;i++){
+            if(word[i] == word[i+1]){
+                count ++;
+            }
+        }
+        return count;
+    }
+};
