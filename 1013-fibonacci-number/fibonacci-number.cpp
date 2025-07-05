090e962fe7eb038633c47ed5a9439ca5caca4881
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-07-05 11:40:49 +0530

    Time: 3 ms (66.93%) | Memory: 8 MB (21.32%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     int fib(int n) {
         if(n == 0) return 0;
-        if(n == 1 || n == 2) return 1;
-        return(fib(n-1)+fib(n-2));
+        if(n == 1 || n == 2) return 1; 
+        vector<int> fib = {0,1,1};
+        for(int i = 3; i <= n;i++){
+            fib.push_back(fib[i-1]+fib[i-2]);
+        }
+        return fib.back();
     }
 };
