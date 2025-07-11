2011c0dc62c053310fba4acece9ba4af5da3bb7e
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-07-11 09:56:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (35.19%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0 ; i*2 < n ;i++){
+            swap(matrix[i] , matrix[n-i-1]);
+        }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0; j < matrix[0].size();j++){
+                if(j < i)
+                    swap(matrix[i][j] , matrix[j][i]);
+            }
+        }
+    }
+};
