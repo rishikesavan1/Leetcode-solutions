d196aefc1d515804d9b336898f21753fe73cdddf
Rishikesavan A <144305013+Rishikesavan1@users.noreply.github.com>
2025-08-11 17:05:14 +0530

    Time: 10 ms (86.17%) | Memory: 147.6 MB (69.68%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        vector<int> res;
-        vector<long> pow2;
-        int mod = 1e9 + 7 , i = 0;
+    const int mod = 1e9+7;
 
-        while(n){
-            if(n & 1)   pow2.push_back(1L << i);
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> p2, res;
+        int i = 0, k = 0;
+        while (n) {
+            if (n & 1) {
+                p2.push_back(i);
+                if (k) p2[k] += p2[k - 1]; // prefix sum of exponents
+                k++;
+            }
             n >>= 1;
             i++;
         }
 
-        for(auto &q : queries){
-            long long prod = 1;
-            for(int j = q[0] ; j <= q[1];j++){
-                prod = (pow2[j] * prod) % mod;
-            }
-            res.push_back(prod);
+        for (auto &q : queries) {
+            int p = p2[q[1]] - (q[0] ? p2[q[0] - 1] : 0);
+            res.push_back(fastPow(2, p));
         }
+        return res;
+    }
 
+private:
+    int fastPow(int base, int exp) {
+        int res = 1;
+        while (exp) {
+            if (exp & 1) res = (1LL * res * base) % mod;
+            base = (1LL * base * base) % mod;
+            exp >>= 1;
+        }
         return res;
     }
 };
