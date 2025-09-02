cea1e936635f0c4779b3203aaacb529df0113b53
Rishikesavan.A <144305013+rishikesavan1@users.noreply.github.com>
2025-09-02 20:05:37 +0530

    Time: 7 ms (66.67%) | Memory: 32.6 MB (74.81%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -21,10 +21,10 @@ public:
     }
 };
 
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
+// auto init = []()
+// { 
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 'c';
+// }();
