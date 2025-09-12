9a72efb799496a944f0aecd190d15d6f71aff63a
Rishikesavan.A <144305013+rishikesavan1@users.noreply.github.com>
2025-09-12 20:42:30 +0530

    Time: 16 ms (15.11%) | Memory: 19.2 MB (25.75%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -3,11 +3,9 @@ public:
     bool doesAliceWin(string s) {
         int vowel_count = 0;
         string vowels = "aeiou";
-        for(auto c : s){
-            if(vowels.find(c) != string::npos){
+        for(auto c : s)
+            if(vowels.find(c) != string::npos)
                 vowel_count++;
-            }
-        }
         return vowel_count ? true : false;
     }
 };
